96ad85db00bd991c5c539a43651ccde1d86905fc
SeoMinSeok6672 <aa01055726672@gmail.com>
2025-03-11 12:42:05 +0900

    Add files via upload

---
+++ 0311/0311 hw/profile.jsx
@@ -0,0 +1,14 @@
+import './Profile.css';
+
+const Profile = () => {
+  return (
+    <div className="profile-container">
+      <h1>서민석 소개</h1>
+      <p><strong>이름:</strong> 서민석</p>
+      <p><strong>학과:</strong> (여기에 학과 입력)</p>
+      <p><strong>관심사:</strong> 리액트, 소프트웨어 개발, 게임 프로그래밍</p>
+    </div>
+  );
+};
+
+export default Profile;
