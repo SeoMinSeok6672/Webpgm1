80413db8fe8b7b79fc76edb7e15208f0f5c923a8
SeoMinSeok6672 <aa01055726672@gmail.com>
2025-04-15 09:37:30 +0900

    Create index.js

---
+++ 0415/useEffect/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Counter3 from './chapter_07/Counter3';
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<Counter3 />);
